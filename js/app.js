771dd8f6d7f5b82686b490fa9ebf635368fe70d5
Raiyan109 <kraiyan109@gmail.com>
2022-03-01 08:02:48 +0600

    api  added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 const searchBtn = () => {
-    console.log('jhkjh')
+    const searchBox = document.getElementById('search-box').value
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchBox}`
+    console.log(url)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showPhoneDetails(data.data))
+}
+const showPhoneDetails = (phones) => {
+    console.log(phones)
 }
